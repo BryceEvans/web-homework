8505cb97d41c6ede057df8c592f4a85d574d3631
BryceEvans <evans.bryce@gmail.com>
2019-08-13 22:23:22 -0600

    More react done, researching graphql

--- webapp/src/transactions/transaction.js
+++ webapp/src/transactions/transaction.js
@@ -1,39 +1,66 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 // import { Link } from 'react-router-dom'
 
-export function Transaction () {
-  return (
-    <Fragment>
-      {/* <Link to='/another'>Another route</Link> */}
-      {/* <div>Content for /another route</div> */}
-      <div>Add Transaction</div>
-      <form>
-        <label>
-          Name:
-          <input type='text' />
-        </label>
-        <label>
-          Amount:
-          <input type='number' />
-          <select>
-            <option value='credit'>
-              Credit
-            </option>
-            <option value='debit'>
-              Debit
-            </option>
-          </select>
-        </label>
-        {/* <label>
-          Description:
-          <input type='text' />
-        </label>
-        <label>
-          Merchant:
-          <input type='text' />
-        </label> */}
-        <input type='submit' value='Submit' />
-      </form>
-    </Fragment>
-  )
+export class Transaction extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      merchant: '',
+      amount: '',
+      description: '',
+      isCredit: true,
+      isDebit: false
+    }
+    // this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (event) {
+    console.log('EVENT Value', event.target.value)
+    this.setState({ value: event.target.value })
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <div>
+          {/* <Link to='/another'>Another route</Link> */}
+          {/* <div>Content for /another route</div> */}
+          <div>Add Transaction</div>
+          <form>
+            {/* <label>
+              Name:
+              <input type='text' />
+            </label> */}
+            <label>
+              Merchant:
+              <input onChange={(e) => this.handleChange(e)} placeholder='Enter Merchant' type='text' value={this.state.merchant.value} />
+            </label>
+            <label>
+              Amount:
+              <input onChange={(e) => this.handleChange(e)} placeholder='Enter Amount' type='number' value={this.state.amount.value} />
+              <select >
+                <option value='Credit'>
+                  Credit
+                </option>
+                <option value='Debit'>
+                  Debit
+                </option>
+              </select>
+              {/* <input name='gender' type='radio' value='debit'>
+                Debit
+              </input>
+              <input name='gender' type='radio' value='credit'>
+                Credit
+              </input> */}
+            </label>
+            <label>
+              Description:
+              <input onChange={(e) => this.handleChange(e)} placeholder='Enter Description' type='text' value={this.state.description.value} />
+            </label>
+            <input type='submit' value='Submit' />
+          </form>
+        </div>
+      </Fragment>
+    )
+  }
 }
